50ddad645aaf43fa3c2a9223f796512969ee9ec3
Sreejith <uksreejith1992@gmail.com>
2020-07-01 15:55:19 +0530

    Add project files.

---
+++ LatestVersionPlugInTest/LatestVersionPlugInTest/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.LatestVersion;
+using Xamarin.Forms;
+
+namespace LatestVersionPlugInTest
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        private async void btnTest_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string latestVersionNumber = await CrossLatestVersion.Current.GetLatestVersionNumber();
+                string installedVersionNumber = CrossLatestVersion.Current.InstalledVersionNumber;
+                bool isLatest = await CrossLatestVersion.Current.IsUsingLatestVersion();
+
+                var displayText = $"Latest Version: {latestVersionNumber} | Installed Version: {installedVersionNumber} | Is Latest: {isLatest}";
+
+                await DisplayAlert("Test", displayText, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Test", ex.Message, "OK");
+            }
+        }
+
+        private async void btnOpenStore_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await CrossLatestVersion.Current.OpenAppInStore();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Test", ex.Message, "OK");
+            }
+        }
+    }
+}
